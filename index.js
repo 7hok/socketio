ad54d51c7f194f0b044a85075d5c33e69974db74
sok menghok <sok.menghok1@gmail.com>
2019-06-27 19:37:27 +0700

    create emit to server and server response back

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app =require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html')
 })
@@ -7,6 +8,19 @@ app.get('/',function(req,res){
 app.get('/home',function(req,res){
     res.send('<h2>testing home </h2>');
 })
+
+io.on('connection',function(socket){
+    console.log('user connected')
+
+    socket.on('chat message',function(data){
+        console.log(`USER :`+ data);
+            io.emit('chat message',data);
+    })
+    socket.on('disconnect',function(){
+        console.log(`one left now`);
+    })
+})
+
 
 http.listen(7272,function(){
     console.log('listening on *: 3000');
